9787a3a40714dbb3d00062f7ea32169235447a2f
adityamore-dev <adityamore.tech@gmail.com>
2026-07-06 23:00:55 +0530

    Add LeetCode 739 Daily Tempratures

---
+++ Stack/DailyTemperatures.java
@@ -0,0 +1,31 @@
+// LeetCode 739 - Daily Temperatures
+// Topic: Monotonic Stack
+// Time Complexity: O(n)
+
+
+import java.util.*;
+
+public class DailyTemperatures {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] answer = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() &&
+                    temperatures[i] > temperatures[stack.peek()]) {
+
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
